b4b1c78c69a74a333263be22ff9716715e702c5d
huayingnami <email.huaying.wang@gmail.com>
2021-04-29 15:18:11 +0200

    [task 5] add Repository annotation

--- src/main/java/com/udacity/jdnd/course3/critter/persistence/repository/CustomerRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/persistence/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.udacity.jdnd.course3.critter.persistence.repository;
 
 import com.udacity.jdnd.course3.critter.persistence.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
--- src/main/java/com/udacity/jdnd/course3/critter/persistence/repository/EmployeeRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/persistence/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.udacity.jdnd.course3.critter.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<EmployeeRepository, Long> {
 }
